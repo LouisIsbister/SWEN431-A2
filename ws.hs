4f5c3977439eccb76fecfd46476b17986201ee51
LouisIsbister <louis.isbister@gmail.com>
2025-05-08 22:47:48 +1200

    Remade parser to only used standard libraries :(

--- ws.hs
+++ ws.hs
@@ -4,13 +4,13 @@ import System.IO ()
 import Control.Monad ()
 
 -- my imports
-import Text.Regex.Posix ((=~))
-import Data.Char (isDigit, toUpper, toLower)
-import Data.List (nub, sortBy)
-import GHC.Float (powerDouble)
 import Data.Bits (Bits(xor, shiftL, shiftR, complement))
+import Data.Char (isDigit, toUpper, toLower)
 import Data.Fixed (mod')
+import Data.List (nub, sortBy, isPrefixOf, takeWhile, dropWhile)
 import Data.Ord (comparing, Down (Down))
+
+import GHC.Float (powerDouble)
 
 
     -------------------- 
@@ -30,14 +30,13 @@ main = do
     let rawTokens = tokenize sourceCode -- tokenise the input
     let tokens = generateArraysAndLambdas rawTokens
 
-    -- stack generation
-    let outputStack = execute tokens EmptyStack -- execute the stack
+    -- token and stack execution
+    let outputStack = execute tokens EmptyStack
     let stackAsList = stackToList outputStack
 
     -- write results to file
-    let writeRes = map show stackAsList  -- get each StackElement as a string
-    writeFile outFName (unlines writeRes)      -- write each elements with a newline on it
-
+    let writeRes = map show stackAsList   -- get each StackElement as a string
+    writeFile outFName (unlines writeRes) -- write each elements with a newline on it
 
 -- string utility functions 
 rmNewLines :: String -> String
@@ -76,10 +75,10 @@ data StackElement = SDInt Int | SDDouble Double | SDString String | SDBool Bool 
     SDVector [StackElement] | SDMatrix [[StackElement]] |
     SDLambda Int [StackElement] | SDVar String |           -- lambda types
     ManipFunc String | BinFunc String | UnaryFunc String | -- function types
-    Syntax String | Ignorable String -- syntax types
+    Syntax String | Ignorable |
+    ParseFail
     deriving Eq
 
--- 
 instance Show StackElement where
     show :: StackElement -> String
     show (SDInt x) = show x
@@ -93,7 +92,8 @@ instance Show StackElement where
     show (ManipFunc str) = str ; show (BinFunc str) = str
     show (UnaryFunc str) = str ; show (Syntax str) = str
 
-    show (Ignorable _) = error "Ignorable should have been ignored by tokenizer!"
+    show Ignorable = error "Ignorable should have been ignored by tokenizer!"
+    show ParseFail = error "ParseFail detected. Parsing process failed to identify a token!"
 
 -- functions for displying a SDVector
 showSDVector :: [StackElement] -> String
@@ -114,49 +114,149 @@ showSDMat [x] acc = showSDMat [] (acc ++ showSDVector x)
 showSDMat (vec : xs) acc = showSDMat xs (acc ++ showSDVector vec ++ ", ")
 
 
-
     ----------------------------------
     --- Parsing/Tokenizing section ---
     ----------------------------------
 
+-- methods to iterate through the input string, generating tokens until it
+-- is it empty
 tokenize :: String -> [StackElement]
 tokenize input = tokenizeHelper input []
 
 tokenizeHelper :: String -> [StackElement] -> [StackElement]
 tokenizeHelper [] tokens = tokens
 tokenizeHelper input tokens =
-    let (token, match) = tryParse input tokenParsers   -- parse token
-        newInput = rmPrefix match input   -- remove matched string from input
-
-    -- recursively tokenize, if its a whitespace or comma (Ignorable) ignore the token!
+    let (token, newInput) = tryParse input parsers   -- parse token
     in case token of
-        Ignorable _ -> tokenizeHelper newInput tokens
-        _ -> tokenizeHelper newInput (tokens ++ [token])
+        Ignorable -> tokenizeHelper newInput tokens
+        _         -> tokenizeHelper newInput (tokens ++ [token])
 
 -- function takes the input, tries to match the top pattern. If it does match it
 -- converts the match to a StackElement, otherwise go to the next pattern!
-tryParse :: String -> [(String, String -> StackElement)] -> (StackElement, String)
-tryParse input ((pattern, func) : xs) =
-    let match = input =~ pattern :: String
-    in case match of
-        "" -> tryParse input xs     -- if its an empty match we try parse the next pattern!
-        _ -> (func match, match)    -- otherwise return a StackElement and match string
+tryParse :: String -> [String -> (String, StackElement)] -> (StackElement, String)
+tryParse input (parser : ps) =
+    let (newInput, token) = parser input
+    in case token of
+        ParseFail -> tryParse input ps
+        _         -> (token, newInput)
 
-tokenParsers :: [(String, String -> StackElement)]
-tokenParsers = [
-    ("^(DROP|DUP|SWAP|ROT|ROLLD|ROLL|IFELSE|EVAL|SELF|TRANSP)", ManipFunc),
-    ("^(\"[^\"]*\")", SDString),
-    ("^(true|false)", SDBool . read . upperFirst),
-    ("^(-?[0-9]+\\.[0-9]+)", SDDouble . read),
-    ("^(0|-?[1-9][0-9]*)", SDInt . read),
-    ("^(x[0-9]+)", SDVar),
-    ("^(\\*\\*|\\+|\\/|\\-|\\*|%|\\^)", BinFunc),
-    ("^(&|\\||==|!=|<<|>>|<=>|>=|<=|>|<|x)", BinFunc),
-    ("^(!|~)", UnaryFunc),
-    ("^(\\[|\\]|\\{|\\}|')", Syntax),
-    ("^(\\s+|,)", Ignorable)
+-- return a list of functions, each takes an input string, tries to match the start 
+-- of that string to a specific pattern. If successful, remove the match from the start
+-- of the input and return a tuple with the new input and the StackElement generated
+-- form the match. If it fails it will always return the orginal input and a ParseFail!
+parsers :: [String -> (String, StackElement)]
+parsers = [
+        parseManipFunc,
+        parseString,
+        parseBool, parseNumeric, parseVar,
+        parseBinFunc, parseUnaryFunc,
+        parseSyntax, parseIgnorable
     ]
 
+-- parsing of stack manipulation functions 
+parseManipFunc :: String -> (String, StackElement)
+parseManipFunc input = parseManipF input ["DROP","DUP","SWAP","ROT","ROLLD","ROLL","IFELSE","EVAL","SELF","TRANSP"]
+
+parseManipF :: String -> [String] -> (String, StackElement)
+parseManipF input [] = (input, ParseFail)
+parseManipF input (mp : mps)
+    | mp `isPrefixOf` input = (drop (length mp) input, ManipFunc mp)
+    | otherwise = parseManipF input mps
+
+-- String parsing, if the first char is " then take evything until next "!
+parseString :: [Char] -> (String, StackElement)
+parseString (firstChar : rest)
+    | firstChar == '\"' = handleString rest (takeWhile (/= '\"') rest)
+    | otherwise = (firstChar : rest, ParseFail)
+
+handleString :: [Char] -> [Char] -> (String, StackElement)
+handleString input captured = (drop (length captured + 1) input, SDString ("\"" ++ captured ++ "\""))
+
+-- parse boolean
+parseBool :: String -> (String, StackElement)
+parseBool input
+    | "true" `isPrefixOf` input = (drop 4 input, SDBool True)
+    | "false" `isPrefixOf` input = (drop 5 input, SDBool False)
+    | otherwise = (input, ParseFail)
+
+-- parse numeric function checks to see if the first char is an int. If so, get it
+-- and then call the parse floating point function which will return a SDDouble if
+-- the next char is a '.', otherwise it will just return an SDInt with the number matched at first
+parseNumeric :: String -> (String, StackElement)
+parseNumeric (firstChar : rest)
+    | isDigit firstChar =
+        let (captureLen, num) = parseNumber rest
+            newRest = drop captureLen rest
+        in parseFloatingPoint (firstChar : num) newRest
+    | firstChar == '-' =
+        case parseNumeric rest of
+            (newRest, SDInt x)    -> (newRest, SDInt (-x))
+            (newRest, SDDouble x) -> (newRest, SDDouble (-x))
+            (_, ParseFail)        -> ('-' : rest, ParseFail)
+    | otherwise = (firstChar : rest, ParseFail)
+
+parseFloatingPoint :: String -> String -> (String, StackElement)
+parseFloatingPoint wholeNum (firstChar : rest)
+    | firstChar == '.' =
+        let (captureLen, decimal) = parseNumber rest
+            newRest = drop captureLen rest
+        in if captureLen == 0
+            then (newRest, SDDouble (read wholeNum :: Double)) -- if the .0 is missing, i.e. 123. instead of 123.0
+            else (newRest, SDDouble (read (wholeNum ++ "." ++ decimal)))
+    | otherwise = (firstChar : rest, SDInt (read wholeNum)) -- default is an int as it has already been parsd
+
+parseNumber :: String -> (Int, String)  -- while there are digits take them, return the length of the captured num and its string
+parseNumber input =
+    let capturedNum = takeWhile isDigit input
+    in (length capturedNum, capturedNum)
+
+-- try parse a variable!
+parseVar :: String -> (String, StackElement)
+parseVar input
+    | "x" `isPrefixOf` input =
+        let (numLen, num) = parseNumber (tail input)  -- retrieve digits after the 'x' of a variable
+        in if numLen == 0   -- if there were none then it was a cross prodcut and we should fail!
+            then (input, ParseFail)
+            else (drop (numLen + 1) input, SDVar ('x' : num))
+    | otherwise = (input, ParseFail)
+
+-- parsing binary oprators
+parseBinFunc :: String -> (String, StackElement)
+parseBinFunc input =
+    parseBinF input ["**", "+", "/", "-", "*", "%", "^", "&", "|", "==", "!=", "<<", ">>", "<=>", ">=", "<=", ">", "<", "x"]
+
+parseBinF :: String -> [String] -> (String, StackElement)
+parseBinF input [] = (input, ParseFail)
+parseBinF input (bf : bfs)
+    | bf `isPrefixOf` input = (drop (length bf) input, BinFunc bf)
+    | otherwise = parseBinF input bfs
+
+-- unary function parsing
+parseUnaryFunc :: String -> (String, StackElement)
+parseUnaryFunc (firstChar : rest)
+    | firstChar == '!' = (rest, UnaryFunc "!")
+    | firstChar == '~' = (rest, UnaryFunc "~")
+    | otherwise = (firstChar : rest, ParseFail)
+
+-- parse each piece of syntax
+parseSyntax :: String -> (String, StackElement)
+parseSyntax input = parseSyn input ["[", "]", "{", "}", "'"]
+
+parseSyn :: String -> [String] -> (String, StackElement)
+parseSyn input [] = (input, ParseFail)
+parseSyn input (s : ss)
+    | s `isPrefixOf` input = (drop 1 input, Syntax s)
+    | otherwise = parseSyn input ss
+
+-- parse tokens that will be discarded
+parseIgnorable :: String -> (String, StackElement)
+parseIgnorable (firstChar : rest)
+    | firstChar == ',' = (rest, Ignorable)
+    | firstChar == ' ' = (dropWhile (== ' ') rest, Ignorable)
+    | otherwise = (firstChar : rest, ParseFail)
+
+
+-- Parsing Lambdas, Vectors, & Matrices
 generateArraysAndLambdas :: [StackElement] -> [StackElement]
 generateArraysAndLambdas tokens = parseArrayAndLambda tokens []
 
@@ -166,26 +266,25 @@ generateArraysAndLambdas tokens = parseArrayAndLambda tokens []
 -- of an array, or last token of a lambda!
 parseArrayAndLambda :: [StackElement] -> [StackElement] -> [StackElement]
 parseArrayAndLambda [] seen = seen
-parseArrayAndLambda ((Syntax "}") : tokens) seen = 
+parseArrayAndLambda ((Syntax "}") : tokens) seen =
     parseArrayAndLambda tokens (parseLambda (reverse seen))
-parseArrayAndLambda ((Syntax "]") : tokens) seen = 
+parseArrayAndLambda ((Syntax "]") : tokens) seen =
     parseArrayAndLambda tokens (parseArray (reverse seen))
 parseArrayAndLambda (t : tokens) seen = parseArrayAndLambda tokens (seen ++ [t]) -- append to the end of seen to maintain order
 
--- parsing arrays and matrices
-parseArray :: [StackElement] -> [StackElement]
-parseArray tokens = parseArrayTokens tokens []
-
 -- generate a list of tokens until the opening brace is hit. At this point create either 
 -- an SDVector or SDMatrix and reverse the seen tokens again! 
+parseArray :: [StackElement] -> [StackElement]
+parseArray tokens = reverse (parseArrayTokens tokens [])
+
 parseArrayTokens :: [StackElement] -> [StackElement] -> [StackElement]
-parseArrayTokens ((Syntax "[") : xs) arrayBuilder = reverse (makeArrayLike arrayBuilder : xs)
+parseArrayTokens ((Syntax "[") : xs) arrayBuilder = makeArrayLike arrayBuilder : xs
 parseArrayTokens (x : xs) arrayBuilder = parseArrayTokens xs (x : arrayBuilder)
 
 -- if the array has vecs as args, then it must be a matrix!
 makeArrayLike :: [StackElement] -> StackElement
 makeArrayLike arr
-    | isMatrix arr = SDMatrix [ vec | (SDVector vec) <- arr] -- extract all the vectors from the SDVectors
+    | isMatrix arr = SDMatrix [vec | (SDVector vec) <- arr] -- extract all the vectors from the SDVectors
     | otherwise = SDVector arr
 
 isMatrix :: [StackElement] -> Bool -- if the first element of an array is a vector it must be a Matrix
@@ -194,24 +293,24 @@ isMatrix _ = False
 
 -- parsing lambdas
 parseLambda :: [StackElement] -> [StackElement]
-parseLambda tokens = parseLambdaTokens tokens []
+parseLambda tokens = reverse (parseLambdaTokens tokens [])
 
 parseLambdaTokens :: [StackElement] -> [StackElement] -> [StackElement]
--- recall the lambda tokens have been reversed, so the based case is '| Int {', which is normally '{ Int |'
-parseLambdaTokens (BinFunc "|" : SDInt paramCount : Syntax "{" : xs) tokenBuilder = 
-    reverse (SDLambda paramCount tokenBuilder : xs)
-parseLambdaTokens (x : xs) tokenBuilder = parseLambdaTokens xs (x : tokenBuilder)
+parseLambdaTokens (BinFunc "|" : SDInt paramX : Syntax "{" : xs) lTokens = SDLambda paramX lTokens : xs
+parseLambdaTokens (x : xs) lTokens = parseLambdaTokens xs (x : lTokens)
 
 
     -------------------------------
     --- Stack execution section ---
     -------------------------------
 
--- take the list of tokens, executing each one as you go
+-- take the list of tokens, executing each one as you go. If the top of 
+-- the stack is quoted then push the token without execution. Otherwise,
+-- execute the next token!
 execute :: [StackElement] -> Stack -> Stack
 execute [] stack = stack
-execute (token : ts) (Top (Syntax "'") stack) = execute ts (push token stack) -- if top is ' quoted then push without execution!
-execute (token : ts) stack = execute ts (executeToken token stack)   -- execute the top token of the stack
+execute (token : ts) (Top (Syntax "'") stack) = execute ts (push token stack)
+execute (token : ts) stack = execute ts (executeToken token stack)
 
 -- match the token and execute it
 executeToken :: StackElement -> Stack -> Stack
@@ -272,10 +371,10 @@ applyUnaryOp (Top t stack) func = push (func t) stack
 -- its takes to functions as args, one to handle integer operations, and to handle double operations.
 -- if both args are ints, it will apply the integer function and return an SDInt
 -- otherwise, one (or both) of the args are doubles, and should return a SDDouble!
-dispatchNumBinFunc :: (Int -> Int -> Int) 
-    -> (Double -> Double -> Double) 
-    -> StackElement 
-    -> StackElement 
+dispatchNumBinFunc :: (Int -> Int -> Int)
+    -> (Double -> Double -> Double)
+    -> StackElement
+    -> StackElement
     -> StackElement
 dispatchNumBinFunc ifunc _ (SDInt l) (SDInt r) = SDInt (ifunc l r)
 dispatchNumBinFunc _ dfunc (SDInt l) (SDDouble r) = SDDouble (dfunc (toDouble l) r)
@@ -284,10 +383,10 @@ dispatchNumBinFunc _ dfunc (SDDouble l) (SDDouble r) = SDDouble (dfunc l r)
 
 -- Exactly the same as the previous method, except this one handles the binary functiosn that 
 -- take two numbers and return booleans instead of numbers again!
-dispatchBoolBinFunc :: (Int -> Int -> Bool) 
-    -> (Double -> Double -> Bool) 
-    -> StackElement 
-    -> StackElement 
+dispatchBoolBinFunc :: (Int -> Int -> Bool)
+    -> (Double -> Double -> Bool)
+    -> StackElement
+    -> StackElement
     -> StackElement
 dispatchBoolBinFunc ifunc _ (SDInt l) (SDInt r) = SDBool (ifunc l r)
 dispatchBoolBinFunc _ dfunc (SDInt l) (SDDouble r) = SDBool (dfunc (toDouble l) r)
@@ -305,7 +404,7 @@ bAdd l r = dispatchNumBinFunc (+) (+) l r  -- all other addition
 
 -- binary subtraction
 bSub :: StackElement -> StackElement -> StackElement
-bSub = dispatchNumBinFunc (-) (-)
+bSub l r = dispatchNumBinFunc (-) (-) l r
 
 -- binary multiplication
 bMul :: StackElement -> StackElement -> StackElement
@@ -327,7 +426,7 @@ bMod (SDDouble l) (SDDouble r) = SDDouble (l `mod'` r)
 
 -- binary exponentiation operation
 bPow :: StackElement -> StackElement -> StackElement
-bPow = dispatchNumBinFunc (^) powerDouble
+bPow l r = dispatchNumBinFunc (^) powerDouble l r
 
 -- binary XOR
 bXor :: StackElement -> StackElement -> StackElement
@@ -425,7 +524,6 @@ transp ([]:_) = []
 transp x = (map head x) : transp (map tail x)
 
 
-
     -------------------------
     --- Lambda Execution! ---
     -------------------------
@@ -440,21 +538,21 @@ executeLambda (SDLambda paramCount tokens) stack =
 -- replace all lambda var and SELF tokens with their respective values, and return the resulting tokens!
 updateLambdaTokens :: Int -> [StackElement] -> Stack-> [StackElement]
 updateLambdaTokens paramCount tokens stack =
-    replaceVars 
+    replaceVars
         (replaceSELF tokens (SDLambda paramCount tokens))    -- first replace all SELF's with the recursive call!
         (makeVarDictionary (distinctVariables tokens) stack) -- generate list of (SDVar, StackElement) tuples
-
--- replaces all variables with thei respective value!
-replaceVars :: [StackElement] -> [(StackElement, StackElement)] -> [StackElement]
-replaceVars [] _ = []
-replaceVars (SDVar varName : ts) varDict = (getVarValue varName varDict) : replaceVars ts varDict
-replaceVars (t : ts) varDict             = t : replaceVars ts varDict
 
 -- replaces all occurrences of SELF with a clone of the original lambda!
 replaceSELF :: [StackElement] -> StackElement -> [StackElement]
 replaceSELF [] _ = []
 replaceSELF (ManipFunc "SELF" : ts) clone = Syntax "'" : clone : replaceSELF ts clone
 replaceSELF (t : ts) clone                = t : replaceSELF ts clone
+
+-- replaces all variables with thei respective value!
+replaceVars :: [StackElement] -> [(StackElement, StackElement)] -> [StackElement]
+replaceVars [] _ = []
+replaceVars (SDVar varName : ts) varDict = getVarValue varName varDict : replaceVars ts varDict
+replaceVars (t : ts) varDict             = t : replaceVars ts varDict
 
 -- given a var name, and var 'dictionary', return the value of the var
 getVarValue :: String -> [(StackElement, StackElement)] -> StackElement
@@ -467,13 +565,12 @@ makeVarDictionary :: [StackElement] -> Stack -> [(StackElement, StackElement)]
 makeVarDictionary [] _ = []
 makeVarDictionary (var : tokens) (Top t stack) = (var, t) : makeVarDictionary tokens stack
 
--- retrieves all the variabels and extracts the numbers from them, calls 'nub' to get the unqiue ones,
--- and them sorts them with the largest at the left most position. Then maps each number back top a var! 
+-- retrieves all unique variables, sorts them in descending order by their number! 
 distinctVariables :: [StackElement] -> [StackElement]
 distinctVariables tokens = 
-    map (\e -> SDVar ("x" ++ show e))
-        (sortBy (comparing Down)
-                (nub [getVarNumber varName | SDVar varName <- tokens]))
+    sortBy              -- 'flip' sorts in descending order i.e. x3 -> x2 -> x1 -> x0 
+        (flip (\(SDVar a) (SDVar b) -> compare (getVarNumber a) (getVarNumber b))) 
+        (nub [SDVar varName | SDVar varName <- tokens]) -- retrieve all variables
 
 -- simply remove the 'x' from teh variable name, and convert to an Int
 getVarNumber :: String -> Int
@@ -484,7 +581,7 @@ getVarNumber varName = read (drop 1 varName)
     ---  Stack definition ---
     -------------------------
 
-data Stack = EmptyStack | Top StackElement Stack 
+data Stack = EmptyStack | Top StackElement Stack
     deriving (Eq, Show)
 
 push :: StackElement -> Stack -> Stack
@@ -494,7 +591,7 @@ pop :: Stack -> Stack
 pop stack = popn stack 1
 
 popn :: Stack -> Int -> Stack
-popn (Top t stack) n 
+popn (Top t stack) n
     | n == 0 = push t stack  -- ensures t is not lost if 0 is passed!
     | n == 1 = stack
     | otherwise = popn stack (n-1)
@@ -541,7 +638,7 @@ rollN (Top t1 stack) n
 ifelse :: Stack -> Stack
 ifelse (Top (SDBool cond) (Top f (Top t stack)))
     | cond      = push t stack
-    | otherwise = push f stack 
+    | otherwise = push f stack
 
 eval :: Stack -> Stack
 eval (Top t stack) = executeToken t stack
